4fa3843f22496e4d224d7e31a4a3032db51ed460
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-09-08 01:01:14 -0400

    Added Section 3 folder
    This folder contains the Set Operator sql file

---
+++ The Complete SQL Course 2023/Section 3/The Complete SQL Course 2023 - Section 3 - Set Operators.sql
@@ -0,0 +1,40 @@
+-- Returns all rows for the following species
+select * from bird.california_sightings
+union all
+select * from bird.arizona_sightings
+
+
+-- Returns all unique species
+select common_name, scientific_name, location_of_sighting, sighting_date from bird.california_sightings
+union
+select common_name, scientific_name, sighting_location, sighting_date from bird.arizona_sightings
+
+
+-- Returns all unique combinations of speices from states California & Arizona
+select common_name, scientific_name, location_of_sighting, sighting_date, 'California' as state from bird.california_sightings
+union
+select common_name, scientific_name, sighting_location, sighting_date, 'Arizona' as state from bird.arizona_sightings
+order by scientific_name, state
+
+
+-- Returns all unique combinations of speices from states California, Arizona & Florida
+select common_name, scientific_name, location_of_sighting, sighting_date, 'California' as state from bird.california_sightings
+union all
+select common_name, scientific_name, sighting_location, sighting_date, 'Arizona' as state from bird.arizona_sightings
+union all
+select NULL, scientific_name, locality, sighting_datetime, 'Florida' as state from bird.florida_sightings
+order by scientific_name, state
+
+
+-- Returns all unique customers who have placed multiple orders
+select customer_id from oes.customers
+intersect
+select customer_id from oes.orders
+
+
+-- Returns all unique products that are currently not in stock 
+select product_id from oes.products
+except
+select product_id from oes.inventories
+
+select * from oes.inventories order by product_id desc
