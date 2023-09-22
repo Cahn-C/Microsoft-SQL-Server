80eb592f041cc218058178aad8e65850eac5cf07
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-09-22 00:59:55 -0400

    Update The Complete SQL Course 2023 - Section 6 - Alter Tablessql.sql

--- The Complete SQL Course 2023/Section 6/The Complete SQL Course 2023 - Section 6 - Alter Tablessql.sql
+++ The Complete SQL Course 2023/Section 6/The Complete SQL Course 2023 - Section 6 - Alter Tablessql.sql
@@ -22,8 +22,27 @@ select * from oes.customers
 -- Or
 sp_help 'oes.customers'
 
+
 -- Make the department_name column a UNIQUE constraint to avoid duplicate departments
 ALTER TABLE hcm.departments ADD CONSTRAINT uk_department_name UNIQUE (department_name)
 
 -- Check the records
 sp_help 'hcm.departments'
+
+
+-- Ensure that the salary for the employees is greater than 0, meaning when new info gets sumbmitted they can not have a salary of 0
+ALTER TABLE hcm.employees ADD CONSTRAINT ck_salary CHECK (salary >= 0)
+
+-- Check the records
+sp_help 'hcm.employees'
+
+-- Check the constraints for a certain schema
+select tc.CONSTRAINT_SCHEMA, tc.CONSTRAINT_NAME, tc.CONSTRAINT_TYPE, ccu.TABLE_SCHEMA, ccu.TABLE_NAME 
+from INFORMATION_SCHEMA.TABLE_CONSTRAINTS tc
+join INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE ccu
+on tc.CONSTRAINT_SCHEMA = ccu.TABLE_SCHEMA
+and tc.TABLE_CATALOG = ccu.TABLE_CATALOG
+and tc.TABLE_NAME = ccu.TABLE_NAME
+and tc.CONSTRAINT_NAME = ccu.CONSTRAINT_NAME
+where ccu.CONSTRAINT_SCHEMA = 'hcm'
+and tc.CONSTRAINT_NAME like 'ck%'
