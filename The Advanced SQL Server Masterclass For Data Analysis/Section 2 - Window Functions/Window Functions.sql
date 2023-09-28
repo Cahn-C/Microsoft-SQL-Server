5a2ddcbeb166ad12e475a676bd86641dbadb1de8
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-09-28 16:52:41 -0400

    Add New Course

---
+++ The Advanced SQL Server Masterclass For Data Analysis/Section 2 - Window Functions/Window Functions.sql
@@ -0,0 +1,60 @@
+-- Exercise 1
+-- Compare the average rate to each rate of the employees
+select pp.FirstName, 
+	   pp.LastName, 
+	   he.JobTitle, 
+	   hh.Rate, 
+	   avg(hh.Rate) over() as AverageRate
+from Person.Person pp
+join HumanResources.Employee he
+on pp.BusinessEntityID = he.BusinessEntityID
+join HumanResources.EmployeePayHistory hh
+on he.BusinessEntityID = hh.BusinessEntityID
+
+
+-- Exercise 2
+-- Compare the largest rate of all values from every employees rate
+select pp.FirstName, 
+	   pp.LastName, 
+	   he.JobTitle, 
+	   hh.Rate, 
+	   avg(hh.Rate) over() as AverageRate,
+	   max(hh.Rate) over() as MaximumRate
+from Person.Person pp
+join HumanResources.Employee he
+on pp.BusinessEntityID = he.BusinessEntityID
+join HumanResources.EmployeePayHistory hh
+on he.BusinessEntityID = hh.BusinessEntityID
+
+
+-- Exercise 3
+-- Returns the employee's pay rate minus the average of all values for every employee
+select pp.FirstName, 
+	   pp.LastName, 
+	   he.JobTitle, 
+	   hh.Rate, 
+	   avg(hh.Rate) over() as AverageRate,
+	   max(hh.Rate) over() as MaximumRate,
+	   hh.Rate - avg(hh.Rate) over() as DiffFromAvgRate
+from Person.Person pp
+join HumanResources.Employee he
+on pp.BusinessEntityID = he.BusinessEntityID
+join HumanResources.EmployeePayHistory hh
+on he.BusinessEntityID = hh.BusinessEntityID
+
+
+-- Exercise 4
+--
+select pp.FirstName, 
+	   pp.LastName, 
+	   he.JobTitle, 
+	   hh.Rate, 
+	   avg(hh.Rate) over() as AverageRate,
+	   max(hh.Rate) over() as MaximumRate,
+	   hh.Rate - avg(hh.Rate) over() as DiffFromAvgRate,
+	   hh.Rate / (max(hh.Rate) over() * 100) as PercentofMaxRate
+from Person.Person pp
+join HumanResources.Employee he
+on pp.BusinessEntityID = he.BusinessEntityID
+join HumanResources.EmployeePayHistory hh
+on he.BusinessEntityID = hh.BusinessEntityID
