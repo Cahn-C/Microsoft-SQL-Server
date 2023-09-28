ba96fb3550f75494868a91e5f90cbc7235650523
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-09-28 17:06:35 -0400

    Update Window Functions.sql

--- The Advanced SQL Server Masterclass For Data Analysis/Section 2 - Window Functions/Window Functions.sql
+++ The Advanced SQL Server Masterclass For Data Analysis/Section 2 - Window Functions/Window Functions.sql
@@ -1,10 +1,10 @@
 -- Exercise 1
 -- Compare the average rate to each rate of the employees
 select pp.FirstName, 
-	   pp.LastName, 
-	   he.JobTitle, 
-	   hh.Rate, 
-	   avg(hh.Rate) over() as AverageRate
+       pp.LastName, 
+       he.JobTitle, 
+       hh.Rate,
+       avg(hh.Rate) over() as AverageRate
 from Person.Person pp
 join HumanResources.Employee he
 on pp.BusinessEntityID = he.BusinessEntityID
@@ -15,11 +15,11 @@ on he.BusinessEntityID = hh.BusinessEntityID
 -- Exercise 2
 -- Compare the largest rate of all values from every employees rate
 select pp.FirstName, 
-	   pp.LastName, 
-	   he.JobTitle, 
-	   hh.Rate, 
-	   avg(hh.Rate) over() as AverageRate,
-	   max(hh.Rate) over() as MaximumRate
+       pp.LastName, 
+       he.JobTitle,  
+       hh.Rate, 
+       avg(hh.Rate) over() as AverageRate,  
+       max(hh.Rate) over() as MaximumRate
 from Person.Person pp
 join HumanResources.Employee he
 on pp.BusinessEntityID = he.BusinessEntityID
@@ -30,12 +30,12 @@ on he.BusinessEntityID = hh.BusinessEntityID
 -- Exercise 3
 -- Returns the employee's pay rate minus the average of all values for every employee
 select pp.FirstName, 
-	   pp.LastName, 
-	   he.JobTitle, 
-	   hh.Rate, 
-	   avg(hh.Rate) over() as AverageRate,
-	   max(hh.Rate) over() as MaximumRate,
-	   hh.Rate - avg(hh.Rate) over() as DiffFromAvgRate
+       pp.LastName, 
+       he.JobTitle,  
+       hh.Rate, 
+       avg(hh.Rate) over() as AverageRate,
+       max(hh.Rate) over() as MaximumRate, 
+       hh.Rate - avg(hh.Rate) over() as DiffFromAvgRate
 from Person.Person pp
 join HumanResources.Employee he
 on pp.BusinessEntityID = he.BusinessEntityID
@@ -46,13 +46,13 @@ on he.BusinessEntityID = hh.BusinessEntityID
 -- Exercise 4
 -- Compare the pertenage of all employees to each employee pay rate
 select pp.FirstName, 
-	   pp.LastName, 
-	   he.JobTitle, 
-	   hh.Rate, 
-	   avg(hh.Rate) over() as AverageRate,
-	   max(hh.Rate) over() as MaximumRate,
-	   hh.Rate - avg(hh.Rate) over() as DiffFromAvgRate,
-	   hh.Rate / (max(hh.Rate) over() * 100) as PercentofMaxRate
+       pp.LastName, 
+       he.JobTitle, 
+       hh.Rate, 
+       avg(hh.Rate) over() as AverageRate,
+       max(hh.Rate) over() as MaximumRate,
+       hh.Rate - avg(hh.Rate) over() as DiffFromAvgRate,
+       hh.Rate / (max(hh.Rate) over() * 100) as PercentofMaxRate
 from Person.Person pp
 join HumanResources.Employee he
 on pp.BusinessEntityID = he.BusinessEntityID
