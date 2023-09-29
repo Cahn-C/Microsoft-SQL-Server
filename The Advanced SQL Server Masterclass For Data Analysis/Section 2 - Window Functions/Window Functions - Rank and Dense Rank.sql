6c2b3504af12c460d8b95aee412af4c047eee039
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-09-28 22:07:43 -0400

    Add Rank and Dense Rank queries

---
+++ The Advanced SQL Server Masterclass For Data Analysis/Section 2 - Window Functions/Window Functions - Rank and Dense Rank.sql
@@ -0,0 +1,52 @@
+-- Exercise 1
+-- 
+select pp.Name, 
+	   pp.ListPrice, 
+	   ps.Name,
+	   pc.Name,
+	   row_number() over(order by pp.ListPrice desc) as PriceRank,
+	   row_number() over(partition by pc.Name order by pp.ListPrice desc) as CategoryPriceRank,
+	   rank() over(partition by pc.Name order by pp.ListPrice desc) as CategoryPriceRankWithRank,
+	   case when row_number() over(partition by pc.Name order by pp.ListPrice desc) <= 5 then 'Yes' else 'No' end as Top5PriceInCategory
+from Production.Product pp
+join Production.ProductSubcategory ps
+on pp.ProductSubcategoryID = ps.ProductSubcategoryID
+join Production.ProductCategory pc
+on ps.ProductCategoryID = pc.ProductCategoryID
+
+
+
+-- Exercise 2
+-- 
+select pp.Name, 
+	   pp.ListPrice, 
+	   ps.Name,
+	   pc.Name,
+	   row_number() over(order by pp.ListPrice desc) as PriceRank,
+	   row_number() over(partition by pc.Name order by pp.ListPrice desc) as CategoryPriceRank,
+	   rank() over(partition by pc.Name order by pp.ListPrice desc) as CategoryPriceRankWithRank,
+	   dense_rank() over(partition by pc.Name order by pp.ListPrice desc) as CategoryPriceRankWithDenseRank,
+	   case when row_number() over(partition by pc.Name order by pp.ListPrice desc) <= 5 then 'Yes' else 'No' end as Top5PriceInCategory
+from Production.Product pp
+join Production.ProductSubcategory ps
+on pp.ProductSubcategoryID = ps.ProductSubcategoryID
+join Production.ProductCategory pc
+on ps.ProductCategoryID = pc.ProductCategoryID
+
+
+-- Exercise 3
+-- 
+select pp.Name, 
+	   pp.ListPrice, 
+	   ps.Name,
+	   pc.Name,
+	   row_number() over(order by pp.ListPrice desc) as PriceRank,
+	   row_number() over(partition by pc.Name order by pp.ListPrice desc) as CategoryPriceRank,
+	   rank() over(partition by pc.Name order by pp.ListPrice desc) as CategoryPriceRankWithRank,
+	   dense_rank() over(partition by pc.Name order by pp.ListPrice desc) as CategoryPriceRankWithDenseRank,
+	   case when row_number() over(partition by pc.Name order by pp.ListPrice desc) <= 5 then 'Yes' else 'No' end as Top5PriceInCategory
+from Production.Product pp
+join Production.ProductSubcategory ps
+on pp.ProductSubcategoryID = ps.ProductSubcategoryID
+join Production.ProductCategory pc
+on ps.ProductCategoryID = pc.ProductCategoryID
