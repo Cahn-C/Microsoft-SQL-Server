f34ccdc101c4be102084e1fbfc600d7bd7f937e7
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-09-29 01:36:30 -0400

    Update Window Functions - Rank and Dense Rank.sql

--- The Advanced SQL Server Masterclass For Data Analysis/Section 2 - Window Functions/Window Functions - Rank and Dense Rank.sql
+++ The Advanced SQL Server Masterclass For Data Analysis/Section 2 - Window Functions/Window Functions - Rank and Dense Rank.sql
@@ -50,3 +50,24 @@ join Production.ProductSubcategory ps
 on pp.ProductSubcategoryID = ps.ProductSubcategoryID
 join Production.ProductCategory pc
 on ps.ProductCategoryID = pc.ProductCategoryID
+
+
+-- Exercise 3
+-- Get all top 5 products for each category
+with cte as (
+	select pp.Name as ProductName, 
+		   pp.ListPrice, 
+		   ps.Name as CategroryName,
+		   pc.Name as SubcategoryName,
+		   row_number() over(order by pp.ListPrice desc) as PriceRank,
+		   row_number() over(partition by pc.Name order by pp.ListPrice desc) as CategoryPriceRank,
+		   rank() over(partition by pc.Name order by pp.ListPrice desc) as CategoryPriceRankWithRank,
+		   dense_rank() over(partition by pc.Name order by pp.ListPrice desc) as CategoryPriceRankWithDenseRank,
+		   case when row_number() over(partition by pc.Name order by pp.ListPrice desc) <= 5 then 'Yes' else 'No' end as Top5PriceInCategory
+	from Production.Product pp
+	join Production.ProductSubcategory ps
+	on pp.ProductSubcategoryID = ps.ProductSubcategoryID
+	join Production.ProductCategory pc
+	on ps.ProductCategoryID = pc.ProductCategoryID
+)
+select * from cte where Top5PriceInCategory = 'yes'
