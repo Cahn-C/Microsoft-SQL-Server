b4e2da53fdde3e55776f81b5997710838f4a292a
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-09-29 20:32:40 -0400

    Update Subquries.sql

--- The Advanced SQL Server Masterclass For Data Analysis/Section 3 - Subquries/Subquries.sql
+++ The Advanced SQL Server Masterclass For Data Analysis/Section 3 - Subquries/Subquries.sql
@@ -2,12 +2,12 @@
 -- Display the three most expensive orders for each vendor
 select * from (
 	select PurchaseOrderID, 
-		   VendorID, 
-		   OrderDate, 
-		   TaxAmt, 
-		   Freight,
-		   TotalDue,
-		   row_number() over(partition by VendorID order by TotalDue desc) as PurchaseOrderRank
+               VendorID, 
+	       OrderDate, 
+	       TaxAmt, 
+	       Freight,
+	       TotalDue,
+	       PurchaseOrderRank = row_number() over(partition by VendorID order by TotalDue desc) 
 	from Purchasing.PurchaseOrderHeader
 ) A
 where PurchaseOrderRank <= 3
@@ -17,12 +17,12 @@ where PurchaseOrderRank <= 3
 -- Display the three most expensive orders for each vendor
 select * from (
 	select PurchaseOrderID, 
-		   VendorID, 
-		   OrderDate, 
-		   TaxAmt, 
-		   Freight,
-		   TotalDue,
-		   dense_rank() over(partition by VendorID order by TotalDue desc) as PurchaseOrderRank
+	       VendorID, 
+	       OrderDate, 
+	       TaxAmt, 
+	       Freight,
+	       TotalDue,
+	       PurchaseOrderRank = dense_rank() over(partition by VendorID order by TotalDue desc)
 	from Purchasing.PurchaseOrderHeader
 ) A
 where PurchaseOrderRank <= 3
