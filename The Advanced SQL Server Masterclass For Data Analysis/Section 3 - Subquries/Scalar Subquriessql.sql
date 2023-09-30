7b23bd42d089a750429b146d15ae4666dc26028d
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-09-29 20:14:23 -0400

    Update Scalar Subquriessql.sql

--- The Advanced SQL Server Masterclass For Data Analysis/Section 3 - Subquries/Scalar Subquriessql.sql
+++ The Advanced SQL Server Masterclass For Data Analysis/Section 3 - Subquries/Scalar Subquriessql.sql
@@ -1,19 +1,19 @@
 -- Exercise 1
 -- Compare the max vacation hours to all of the employee's regular vacation hours
 select BusinessEntityID,
-	   JobTitle,
-	   VacationHours,
-	   MaxVacationHours = (select max(VacationHours) from HumanResources.Employee)
+       JobTitle,
+       VacationHours,
+       MaxVacationHours = (select max(VacationHours) from HumanResources.Employee)
 from HumanResources.Employee
 
 
 -- Exercise 2
 -- 
 select BusinessEntityID,
-	   JobTitle,
-	   VacationHours,
-	   MaxVacationHours = (select max(VacationHours) from HumanResources.Employee),
-	   PercentOfVactionHours = (VacationHours * 1.0) / (select max(VacationHours) from HumanResources.Employee)
+       JobTitle,
+       VacationHours,  
+       MaxVacationHours = (select max(VacationHours) from HumanResources.Employee),
+       PercentOfVactionHours = (VacationHours * 1.0) / (select max(VacationHours) from HumanResources.Employee)
 from HumanResources.Employee
 
 
@@ -21,10 +21,10 @@ from HumanResources.Employee
 -- 
 select * from (
 	select BusinessEntityID,
-		   JobTitle,
-		   VacationHours,
-		   MaxVacationHours = (select max(VacationHours) from HumanResources.Employee),
-		   PercentOfVactionHours = (VacationHours * 1.0) / (select max(VacationHours) from HumanResources.Employee)
+	       JobTitle,
+	       VacationHours,
+	       MaxVacationHours = (select max(VacationHours) from HumanResources.Employee),
+	       PercentOfVactionHours = (VacationHours * 1.0) / (select max(VacationHours) from HumanResources.Employee)
 	from HumanResources.Employee
 ) X
 where PercentOfVactionHours >= 0.8
