c26eedffd9cfcd3603126d4be01d53c1f5da79d8
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-09-29 20:20:32 -0400

    Update Scalar Subquriessql.sql

--- The Advanced SQL Server Masterclass For Data Analysis/Section 3 - Subquries/Scalar Subquriessql.sql
+++ The Advanced SQL Server Masterclass For Data Analysis/Section 3 - Subquries/Scalar Subquriessql.sql
@@ -8,7 +8,7 @@ from HumanResources.Employee
 
 
 -- Exercise 2
--- 
+-- Compare the percentage of maximum vacation hours for every employee
 select BusinessEntityID,
        JobTitle,
        VacationHours,  
@@ -18,7 +18,7 @@ from HumanResources.Employee
 
 
 -- Exercise 3
--- 
+-- Get all employees who have at least 80% as much vacation time as the employee with the most vacation time. 
 select * from (
 	select BusinessEntityID,
 	       JobTitle,
