0cb72de1cdfe927bf207dd07f5b1b64816435eb5
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-09-29 20:12:20 -0400

    Update Correlated Subquries.sql

--- The Advanced SQL Server Masterclass For Data Analysis/Section 3 - Subquries/Correlated Subquries.sql
+++ The Advanced SQL Server Masterclass For Data Analysis/Section 3 - Subquries/Correlated Subquries.sql
@@ -1,26 +1,26 @@
 -- Exercise 1
--- 
+-- View the total amount of orders for each purchase order id that has NO rejected quantity
 select PurchaseOrderID,
-	   VendorID,
-	   OrderDate,
-	   TotalDue,
-	   NonRejectedItems = (select count(*) from Purchasing.PurchaseOrderDetail d
-						   where d.PurchaseOrderID = h.PurchaseOrderID
-						   and d.RejectedQty = 0)
+       VendorID,
+       OrderDate,
+       TotalDue,
+       NonRejectedItems = (select count(*) from Purchasing.PurchaseOrderDetail d
+			   where d.PurchaseOrderID = h.PurchaseOrderID
+	                   and d.RejectedQty = 0)
 from Purchasing.PurchaseOrderHeader h
 
 
 -- Exercise 2
--- 
+-- View the total amount of orders that has NO rejected quantity along with the most expensive unit price for each purchase order id
 select PurchaseOrderID,
-	   VendorID,
-	   OrderDate,
-	   TotalDue,
-	   NonRejectedItems = (select count(*) from Purchasing.PurchaseOrderDetail d
-						   where d.PurchaseOrderID = h.PurchaseOrderID
-						   and d.RejectedQty = 0),
-	   MostExpensiveItem = (select max(UnitPrice) from Purchasing.PurchaseOrderDetail d
-							where d.PurchaseOrderID = h.PurchaseOrderID)
+       VendorID,
+       OrderDate,
+       TotalDue,
+       NonRejectedItems = (select count(*) from Purchasing.PurchaseOrderDetail d
+			   where d.PurchaseOrderID = h.PurchaseOrderID
+			   and d.RejectedQty = 0),
+       MostExpensiveItem = (select max(UnitPrice) from Purchasing.PurchaseOrderDetail d
+			    where d.PurchaseOrderID = h.PurchaseOrderID)
 from Purchasing.PurchaseOrderHeader h
 
 select PurchaseOrderID, UnitPrice from Purchasing.PurchaseOrderDetail
