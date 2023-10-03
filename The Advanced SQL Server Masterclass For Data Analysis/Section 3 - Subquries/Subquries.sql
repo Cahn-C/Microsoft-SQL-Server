59e070a0ae24c3a23d3c142a6683d81a450e7716
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-10-02 22:00:54 -0400

    Update Subquries.sql

--- The Advanced SQL Server Masterclass For Data Analysis/Section 3 - Subquries/Subquries.sql
+++ The Advanced SQL Server Masterclass For Data Analysis/Section 3 - Subquries/Subquries.sql
@@ -26,3 +26,19 @@ select * from (
 	from Purchasing.PurchaseOrderHeader
 ) A
 where PurchaseOrderRank <= 3
+
+
+-- Exercise 3
+-- Get the total amount of the top ten orders for each month
+select OrderMonth,
+	   sum(TotalDue) as TotalAmount
+from (
+	select OrderDate,
+		   TotalDue,
+		   OrderMonth = datefromparts(year(OrderDate), month(OrderDate), 1),
+		   OrderRank = row_number() over(partition by datefromparts(year(OrderDate), month(OrderDate), 1) order by TotalDue)
+	from Sales.SalesOrderHeader
+) TopTenOrders
+where OrderRank <= 10
+group by OrderMonth
+order by 1 asc
