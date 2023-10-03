166271a718724f86c242a635e566c33ba975e0f5
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-10-02 22:13:54 -0400

    Update Subquries.sql

--- The Advanced SQL Server Masterclass For Data Analysis/Section 3 - Subquries/Subquries.sql
+++ The Advanced SQL Server Masterclass For Data Analysis/Section 3 - Subquries/Subquries.sql
@@ -42,3 +42,37 @@ from (
 where OrderRank <= 10
 group by OrderMonth
 order by 1 asc
+
+
+-- Compare the total amount of sales and purchases at the start of each month
+select A.OrderMonth,
+	   A.TotalSales,
+	   B.TotalPurchases
+from (
+	select OrderMonth,
+		   sum(TotalDue) as TotalSales
+	from (
+		select OrderDate,
+			   TotalDue,
+			   OrderMonth = datefromparts(year(OrderDate), month(OrderDate), 1),
+			   OrderRank = row_number() over(partition by datefromparts(year(OrderDate), month(OrderDate), 1) order by TotalDue)
+		from Sales.SalesOrderHeader
+	) TopTenOrders
+	where OrderRank <= 10
+	group by OrderMonth
+) A
+left join (
+	select OrderMonth,
+		   sum(TotalDue) as TotalPurchases
+	from (
+		select OrderDate,
+			   TotalDue,
+			   OrderMonth = datefromparts(year(OrderDate), month(OrderDate), 1),
+			   OrderRank = row_number() over(partition by datefromparts(year(OrderDate), month(OrderDate), 1) order by TotalDue)
+		from Purchasing.PurchaseOrderHeader
+	) TopTenOrders
+	where OrderRank <= 10
+	group by OrderMonth
+) B
+on A.OrderMonth = B.OrderMonth
+order by 1 asc
