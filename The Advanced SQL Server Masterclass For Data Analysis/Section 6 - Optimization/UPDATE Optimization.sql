db9ef92b996b9cbee83c7aee13c6d47d0d5fac66
Chaanyah Laborde <72324462+Cahn-C@users.noreply.github.com>
2023-10-06 21:22:40 -0400

    Update UPDATE Optimization.sql

--- The Advanced SQL Server Masterclass For Data Analysis/Section 6 - Optimization/UPDATE Optimization.sql
+++ The Advanced SQL Server Masterclass For Data Analysis/Section 6 - Optimization/UPDATE Optimization.sql
@@ -2,14 +2,14 @@ USE [AdventureWorks2017]
 
 -- Starter code
 SELECT 
-	   A.BusinessEntityID
+       A.BusinessEntityID
       ,A.Title
       ,A.FirstName
       ,A.MiddleName
       ,A.LastName
-	  ,B.PhoneNumber
-	  ,PhoneNumberType = C.Name
-	  ,D.EmailAddress
+      ,B.PhoneNumber
+      ,PhoneNumberType = C.Name
+      ,D.EmailAddress
 
 FROM AdventureWorks2017.Person.Person A
 	LEFT JOIN AdventureWorks2017.Person.PersonPhone B
@@ -40,14 +40,14 @@ INSERT INTO #BusinessRecords2017 (
 )
 SELECT * FROM #BusinessRecords2017
 
--- 
+-- Update the PhoneNumber column in the BusinessRecors2017 table with the phone number records in the Person.PersonPhone table
 UPDATE #BusinessRecords2017
 SET PhoneNumber = B.PhoneNumber
 FROM #BusinessRecords2017 A
 JOIN Person.PersonPhone B
 ON A.BusinessEntityID = B.BusinessEntityID
 
--- 
+-- Update the PhoneNumberType column in the BusinessRecors2017 table with the name records in the Person.PhoneNumberType table
 UPDATE #BusinessRecords2017
 SET PhoneNumberType = C.Name
 FROM #BusinessRecords2017 A
@@ -57,7 +57,7 @@ JOIN Person.PhoneNumberType C
 ON B.PhoneNumberTypeID = C.PhoneNumberTypeID
 
 
--- 
+-- Update the EmailAddress column in the BusinessRecors2017 table with the email address records in the Person.EmailAddress table
 UPDATE #BusinessRecords2017
 SET EmailAddress = B.EmailAddress
 FROM #BusinessRecords2017 A
