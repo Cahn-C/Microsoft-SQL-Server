b00adc48d35a84bbd608b6e14c60d0058203e888
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2023-12-05 05:05:33 -0500

    Update SQL Data Statements.sql

--- Complete SQL End to End/SQL Data Statements.sql
+++ Complete SQL End to End/SQL Data Statements.sql
@@ -31,13 +31,30 @@ INSERT INTO Customer (CustomerID, CustomerName, CustomerAddress) VALUES (1, 'Joh
 
 INSERT INTO Customer (CustomerId, CustomerName, CustomerAddress) 
 VALUES (2, 'Kumar', 'Bangalore'),
-	   (3, 'Abdulia', 'Kuwait')
+       (3, 'Abdulia', 'Kuwait')
+
+INSERT INTO Customer (CustomerId, CustomerName, CustomerAddress, PhoneNumber) VALUES 
+(4, 'Jessica', 'France', '+1 (770) 321-2984'),
+(5, 'Dante', 'Paris', '+1 (770) 321-2984'),
+(6, 'Mathew', 'Egypt', '+1 (770) 321-2984'),
+(7, 'Rebecca', 'Bangelore', '+1 (770) 321-2984'),
+(8, 'Alex', 'New York', '+1 (770) 321-2984'),
+(9, 'David', 'New Jersey', '+1 (770) 321-2984'),
+(10, 'Moeses', 'California', '+1 (770) 321-2984')						
 
 
-UPDATE Customer SET Gender = 'M'
+UPDATE Customer SET Gender = 'M' WHERE CustomerID IN (1, 2, 3, 5, 6, 8, 9, 10)
+UPDATE Customer SET Gender = 'F' WHERE CustomerID NOT IN (1, 2, 3, 5, 6, 8, 9, 10)
 UPDATE Customer SET PhoneNumber = '+1 (678) 440-2034' WHERE CustomerID = 1
-UPDATE Customer SET PhoneNumber = '+91 (1234) 5678-90' WHERE CustomerID = 2
-UPDATE Customer SET PhoneNumber = '+1 (770) 940-2944' WHERE CustomerID = 3
+UPDATE Customer SET PhoneNumber = '+1 (678) 382-1173' WHERE CustomerID = 2
+UPDATE Customer SET PhoneNumber = '+1 (770) 421-3234' WHERE CustomerID = 3
+UPDATE Customer SET PhoneNumber = '+1 (404) 323-3718' WHERE CustomerID = 4
+UPDATE Customer SET PhoneNumber = '+1 (679) 198-1222' WHERE CustomerID = 5
+UPDATE Customer SET PhoneNumber = '+1 (770) 251-1255' WHERE CustomerID = 6
+UPDATE Customer SET PhoneNumber = '+1 (678) 327-4771' WHERE CustomerID = 7
+UPDATE Customer SET PhoneNumber = '+1 (404) 392-1033' WHERE CustomerID = 8
+UPDATE Customer SET PhoneNumber = '+1 (678) 120-3742' WHERE CustomerID = 9
+UPDATE Customer SET PhoneNumber = '+1 (770) 463-3911' WHERE CustomerID = 10
 
 DELETE FROM Customer
 
