3e20b1c2a959e70aa8c13dadc8ea18bcc94ff115
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2024-01-16 04:16:18 -0500

    Update Subquries.sql

--- The Advanced SQL Server Masterclass For Data Analysis/Section 3 - Subquries/Subquries.sql
+++ The Advanced SQL Server Masterclass For Data Analysis/Section 3 - Subquries/Subquries.sql
@@ -1,31 +1,43 @@
 -- Exercise 1
 -- Display the three most expensive orders for each vendor
-select * from (
-	select PurchaseOrderID, 
-               VendorID, 
-	       OrderDate, 
-	       TaxAmt, 
-	       Freight,
-	       TotalDue,
-	       PurchaseOrderRank = row_number() over(partition by VendorID order by TotalDue desc) 
-	from Purchasing.PurchaseOrderHeader
-) A
-where PurchaseOrderRank <= 3
+SELECT PurchaseOrderID,
+       VendorID,
+       OrderDate,
+       TaxAmt,
+       Freight,
+       TotalDue 
+FROM (
+	SELECT PurchaseOrderID,
+               VendorID,
+	       OrderDate,
+               TaxAmt,
+               Freight,
+               TotalDue,
+	       PurchaseOrderRank = ROW_NUMBER() OVER(PARTITION BY VendorID ORDER BY TotalDue DESC)
+FROM [Purchasing].[PurchaseOrderHeader]
+)Purchases
+WHERE PurchaseOrderRank <= 3
 
 
 -- Exercise 2
 -- Display the three most expensive orders for each vendor
-select * from (
-	select PurchaseOrderID, 
-	       VendorID, 
-	       OrderDate, 
-	       TaxAmt, 
+SELECT PurchaseOrderID,
+       VendorID,
+       OrderDate, 
+       TaxAmt,
+       Freight,
+       TotalDue 
+FROM (
+	SELECT PurchaseOrderID,
+	       VendorID,
+	       OrderDate,
+     	       TaxAmt,
 	       Freight,
 	       TotalDue,
-	       PurchaseOrderRank = dense_rank() over(partition by VendorID order by TotalDue desc)
-	from Purchasing.PurchaseOrderHeader
-) A
-where PurchaseOrderRank <= 3
+	       PurchaseOrderRank = DENSE_RANK() OVER(PARTITION BY VendorID ORDER BY TotalDue DESC)
+FROM [Purchasing].[PurchaseOrderHeader]
+) Purchases
+WHERE PurchaseOrderRank <= 3
 
 
 -- Exercise 3
