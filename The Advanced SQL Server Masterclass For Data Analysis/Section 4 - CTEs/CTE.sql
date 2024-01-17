a59837c2b4375b67951a2799c8f350916af137e9
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2024-01-17 04:02:37 -0500

    Update CTE.sql

--- The Advanced SQL Server Masterclass For Data Analysis/Section 4 - CTEs/CTE.sql
+++ The Advanced SQL Server Masterclass For Data Analysis/Section 4 - CTEs/CTE.sql
@@ -1,35 +1,35 @@
 -- Compare the total amount of all sales and purchases at the start of each month
 WITH Sales AS (
 	SELECT OrderDate
-		  ,OrderMonth = DATEFROMPARTS(YEAR(OrderDate),MONTH(OrderDate),1)
-		  ,TotalDue
-		  ,OrderRank = ROW_NUMBER() OVER(PARTITION BY DATEFROMPARTS(YEAR(OrderDate),MONTH(OrderDate),1) ORDER BY TotalDue DESC)
+	       ,OrderMonth = DATEFROMPARTS(YEAR(OrderDate),MONTH(OrderDate),1)
+	       ,TotalDue
+	       ,OrderRank = ROW_NUMBER() OVER(PARTITION BY DATEFROMPARTS(YEAR(OrderDate),MONTH(OrderDate),1) ORDER BY TotalDue DESC)
 	FROM AdventureWorks2019.Sales.SalesOrderHeader
 ),
 MinusTop10Sales AS (
 	SELECT OrderMonth,
-		   TotalSales = SUM(TotalDue)
+	       TotalSales = SUM(TotalDue)
 	FROM Sales
 	WHERE OrderRank > 10
 	GROUP BY OrderMonth
 ),
 Purchases AS (
 	SELECT OrderDate
-		  ,OrderMonth = DATEFROMPARTS(YEAR(OrderDate),MONTH(OrderDate),1)
-		  ,TotalDue
-		  ,OrderRank = ROW_NUMBER() OVER(PARTITION BY DATEFROMPARTS(YEAR(OrderDate),MONTH(OrderDate),1) ORDER BY TotalDue DESC)
+	      ,OrderMonth = DATEFROMPARTS(YEAR(OrderDate),MONTH(OrderDate),1)
+	      ,TotalDue
+	      ,OrderRank = ROW_NUMBER() OVER(PARTITION BY DATEFROMPARTS(YEAR(OrderDate),MONTH(OrderDate),1) ORDER BY TotalDue DESC)
 	FROM AdventureWorks2019.Purchasing.PurchaseOrderHeader
 ),
 MiunsTop10Purchases AS (
 	SELECT OrderMonth,
-		   TotalPurchases = SUM(TotalDue)
+               TotalPurchases = SUM(TotalDue)
 	FROM Purchases
 	WHERE OrderRank > 10
 	GROUP BY OrderMonth
 )
 SELECT S.OrderMonth,
-	   TotalSales,
-	   TotalPurchases
+       TotalSales,
+       TotalPurchases
 FROM MinusTop10Sales S 
 JOIN MiunsTop10Purchases P
 ON S.OrderMonth = P.OrderMonth
