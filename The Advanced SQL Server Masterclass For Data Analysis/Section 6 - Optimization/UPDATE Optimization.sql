5169148ef8369a816800501089d375fe545942f0
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2024-01-18 17:09:59 -0500

    Update UPDATE Optimization.sql

--- The Advanced SQL Server Masterclass For Data Analysis/Section 6 - Optimization/UPDATE Optimization.sql
+++ The Advanced SQL Server Masterclass For Data Analysis/Section 6 - Optimization/UPDATE Optimization.sql
@@ -1,66 +1,73 @@
-USE [AdventureWorks2017]
+USE [AdventureWorks2019]
 
--- Starter code
 SELECT 
-       A.BusinessEntityID
+	   A.BusinessEntityID
       ,A.Title
       ,A.FirstName
       ,A.MiddleName
       ,A.LastName
-      ,B.PhoneNumber
-      ,PhoneNumberType = C.Name
-      ,D.EmailAddress
+	  ,B.PhoneNumber
+	  ,PhoneNumberType = C.Name
+	  ,D.EmailAddress
 
-FROM AdventureWorks2017.Person.Person A
-	LEFT JOIN AdventureWorks2017.Person.PersonPhone B
+FROM AdventureWorks2019.Person.Person A
+	LEFT JOIN AdventureWorks2019.Person.PersonPhone B
 		ON A.BusinessEntityID = B.BusinessEntityID
-	LEFT JOIN AdventureWorks2017.Person.PhoneNumberType C
+	LEFT JOIN AdventureWorks2019.Person.PhoneNumberType C
 		ON B.PhoneNumberTypeID = C.PhoneNumberTypeID
-	LEFT JOIN AdventureWorks2017.Person.EmailAddress D
+	LEFT JOIN AdventureWorks2019.Person.EmailAddress D
 		ON A.BusinessEntityID = D.BusinessEntityID
 
 
--- Code to imporve query performance
-CREATE TABLE #BusinessRecords2017 (
+
+CREATE TABLE #Employees (
 	BusinessEntityID INT,
-	Title VARCHAR(100),
-	FirstName VARCHAR(100),
-	MiddleName VARCHAR(100),
-	LastName VARCHAR(100),
+	Title VARCHAR(5),
+	FirstName VARCHAR(50),
+	MiddleName VARCHAR(50),
+	LastName VARCHAR(50),
+	PhoneNumberTypeID INT,
 	PhoneNumber VARCHAR(19),
 	PhoneNumberType VARCHAR(4),
 	EmailAddress VARCHAR(100)
 )
-INSERT INTO #BusinessRecords2017 (
+
+INSERT INTO #Employees (
 	BusinessEntityID,
 	Title,
 	FirstName,
 	MiddleName,
 	LastName
 )
-SELECT * FROM #BusinessRecords2017
+SELECT A.BusinessEntityID
+      ,A.Title
+      ,A.FirstName
+      ,A.MiddleName
+      ,A.LastName
+FROM AdventureWorks2019.Person.Person A
 
--- Update the PhoneNumber column in the BusinessRecors2017 table with the phone number records in the Person.PersonPhone table
-UPDATE #BusinessRecords2017
-SET PhoneNumber = B.PhoneNumber
-FROM #BusinessRecords2017 A
+
+UPDATE #Employees 
+SET PhoneNumber = B.PhoneNumber,
+    PhoneNumberTypeID = B.PhoneNumberTypeID
+FROM #Employees A 
 JOIN Person.PersonPhone B
 ON A.BusinessEntityID = B.BusinessEntityID
 
--- Update the PhoneNumberType column in the BusinessRecors2017 table with the name records in the Person.PhoneNumberType table
-UPDATE #BusinessRecords2017
-SET PhoneNumberType = C.Name
-FROM #BusinessRecords2017 A
-JOIN Person.PersonPhone B
-ON A.BusinessEntityID = B.BusinessEntityID
-JOIN Person.PhoneNumberType C
-ON B.PhoneNumberTypeID = C.PhoneNumberTypeID
+UPDATE #Employees
+SET PhoneNumberType = B.Name
+FROM #Employees A
+JOIN Person.PhoneNumberType B
+ON A.PhoneNumberTypeID = B.PhoneNumberTypeID
 
 
--- Update the EmailAddress column in the BusinessRecors2017 table with the email address records in the Person.EmailAddress table
-UPDATE #BusinessRecords2017
+UPDATE #Employees 
 SET EmailAddress = B.EmailAddress
-FROM #BusinessRecords2017 A
+FROM #Employees A 
 JOIN Person.EmailAddress B
 ON A.BusinessEntityID = B.BusinessEntityID
 
+
+SELECT * FROM #Employees
+
+DROP TABLE #Employees
