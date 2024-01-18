17a78d7fbb0550833b1364ce893066d5691bb7ff
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2024-01-18 16:46:54 -0500

    Update Subquries - Exists.sql

--- The Advanced SQL Server Masterclass For Data Analysis/Section 3 - Subquries/Subquries - Exists.sql
+++ The Advanced SQL Server Masterclass For Data Analysis/Section 3 - Subquries/Subquries - Exists.sql
@@ -1,18 +1,18 @@
 -- Exercise 1
 SELECT PurchaseOrderID,
-	   OrderDate,
-	   SubTotal,
-	   TaxAmt
+       OrderDate,
+       SubTotal,
+       TaxAmt
 FROM [Purchasing].[PurchaseOrderHeader] POH
 WHERE EXISTS (SELECT 1 FROM [Purchasing].[PurchaseOrderDetail] POD 
-	          WHERE POD.PurchaseOrderID = POH.PurchaseOrderID 
-			  AND OrderQty > 500)
+	      WHERE POD.PurchaseOrderID = POH.PurchaseOrderID 
+	      AND OrderQty > 500)
 ORDER BY PurchaseOrderID
 
 SELECT POH.PurchaseOrderID,
-	   OrderDate,
-	   SubTotal,
-	   TaxAmt
+       OrderDate,
+       SubTotal,
+       TaxAmt
 FROM [Purchasing].[PurchaseOrderHeader] POH
 JOIN [Purchasing].[PurchaseOrderDetail] POD
 ON POH.PurchaseOrderID = POD.PurchaseOrderID
@@ -23,9 +23,9 @@ WHERE POD.OrderQty > 500
 -- Exercise 2
 SELECT * FROM [Purchasing].[PurchaseOrderHeader] POH
 WHERE EXISTS (SELECT 1 FROM [Purchasing].[PurchaseOrderDetail] POD 
-			  WHERE POD.PurchaseOrderID = POH.PurchaseOrderID 
-			  AND OrderQty > 500 
-			  AND UnitPrice > 50)
+	      WHERE POD.PurchaseOrderID = POH.PurchaseOrderID 
+              AND OrderQty > 500 
+	      AND UnitPrice > 50)
 ORDER BY 1
 
 SELECT POH.*
@@ -41,8 +41,8 @@ ORDER BY 1
 -- Exercise 3
 SELECT * FROM [Purchasing].[PurchaseOrderHeader] POH
 WHERE NOT EXISTS (SELECT 1 FROM [Purchasing].[PurchaseOrderDetail] POD 
-				  WHERE POD.PurchaseOrderID = POH.PurchaseOrderID 
-				  AND RejectedQty > 0)
+                  WHERE POD.PurchaseOrderID = POH.PurchaseOrderID 
+		  AND RejectedQty > 0)
 ORDER BY 1
 
 SELECT POH.*
