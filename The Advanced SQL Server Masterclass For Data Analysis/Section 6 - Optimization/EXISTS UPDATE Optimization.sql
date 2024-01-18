8970818e2cedeb348c1a75f1c2894b04822e9be5
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2024-01-18 17:29:24 -0500

    Update EXISTS UPDATE Optimization.sql

--- The Advanced SQL Server Masterclass For Data Analysis/Section 6 - Optimization/EXISTS UPDATE Optimization.sql
+++ The Advanced SQL Server Masterclass For Data Analysis/Section 6 - Optimization/EXISTS UPDATE Optimization.sql
@@ -1,40 +1,49 @@
-USE [AdventureWorks2017]
+USE [AdventureWorks2019]
 
--- Starter Code
 SELECT
        A.PurchaseOrderID,
        A.OrderDate,
        A.TotalDue
 
-FROM AdventureWorks2017.Purchasing.PurchaseOrderHeader A
+FROM AdventureWorks2019.Purchasing.PurchaseOrderHeader A
 
 WHERE EXISTS (
 	SELECT
 	1
-	FROM AdventureWorks2017.Purchasing.PurchaseOrderDetail B
+	FROM AdventureWorks2019.Purchasing.PurchaseOrderDetail B
 	WHERE A.PurchaseOrderID = B.PurchaseOrderID
-		AND B.RejectedQty > 5
+	AND B.RejectedQty > 5
 )
 
 ORDER BY 1
 
 
--- Code that provides query performance when searching for an existing record
-CREATE TABLE #PurchaseOrders2017 (
+CREATE TABLE #Purchases (
 	PurchaseOrderID INT,
 	OrderDate DATE,
-	TotalDue MONEY,
-	RejectedQty INT
+        TotalDue MONEY,
+	RejectedQTY INT
 )
-INSERT INTO #PurchaseOrders2017 (
+
+INSERT INTO #Purchases (
 	PurchaseOrderID,
 	OrderDate,
-	TotalDue
+        TotalDue
 )
-SELECT * FROM #PurchaseOrders2017
+SELECT PurchaseOrderID,
+       OrderDate,
+       TotalDue
+FROM AdventureWorks2019.Purchasing.PurchaseOrderHeader A
 
-UPDATE #PurchaseOrders2017 SET RejectedQty = B.RejectedQty
-FROM #PurchaseOrders2017 A
+UPDATE #Purchases 
+SET RejectedQTY = B.RejectedQty
+FROM #Purchases A
 JOIN Purchasing.PurchaseOrderDetail B
 ON A.PurchaseOrderID = B.PurchaseOrderID
-AND B.RejectedQty > 5
+WHERE B.RejectedQty > 5
+
+
+SELECT * FROM #Purchases WHERE RejectedQTY IS NOT NULL ORDER BY 1
+
+
+DROP TABLE #Purchases
