f402826145c484992fd98256653a13bcb22d5bd7
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2024-02-14 20:18:09 -0500

    Add adventure works 2019 sql file

---
+++ Projects/AdventureWorks2019Project.sql
@@ -0,0 +1,64 @@
+USE [AdventureWorks2019]
+GO
+
+--
+SELECT * FROM [Person].[Person]
+SELECT * FROM [HumanResources].[Employee]
+SELECT * FROM [HumanResources].[EmployeePayHistory]
+
+-- 
+SELECT A.FirstName,
+	   A.LastName,
+	   B.JobTitle,
+	   C.Rate,
+	   AverageRate = AVG(C.Rate) OVER()
+FROM [Person].[Person] A
+JOIN [HumanResources].[Employee] B
+ON A.BusinessEntityID = B.BusinessEntityID
+JOIN [HumanResources].[EmployeePayHistory] C
+ON B.BusinessEntityID = C.BusinessEntityID
+
+
+--
+SELECT A.FirstName,
+	   A.LastName,
+	   B.JobTitle,
+	   C.Rate,
+	   AverageRate = AVG(C.Rate) OVER(),
+	   MaximunRate = MAX(C.Rate) OVER()
+FROM [Person].[Person] A
+JOIN [HumanResources].[Employee] B
+ON A.BusinessEntityID = B.BusinessEntityID
+JOIN [HumanResources].[EmployeePayHistory] C
+ON B.BusinessEntityID = C.BusinessEntityID
+
+
+--
+SELECT A.FirstName,
+	   A.LastName,
+	   B.JobTitle,
+	   C.Rate,
+	   AverageRate = AVG(C.Rate) OVER(),
+	   MaximunRate = MAX(C.Rate) OVER(),
+	   DiffFromAvgRate = C.Rate - AVG(C.Rate) OVER()
+FROM [Person].[Person] A
+JOIN [HumanResources].[Employee] B
+ON A.BusinessEntityID = B.BusinessEntityID
+JOIN [HumanResources].[EmployeePayHistory] C
+ON B.BusinessEntityID = C.BusinessEntityID
+
+
+-- 
+SELECT A.FirstName,
+	   A.LastName,
+	   B.JobTitle,
+	   C.Rate,
+	   AverageRate = AVG(C.Rate) OVER(),
+	   MaximunRate = MAX(C.Rate) OVER(),
+	   DiffFromAvgRate = C.Rate - AVG(C.Rate) OVER(),
+	   PercentageofMaxRate = (C.Rate / MAX(C.Rate) OVER()) * 100
+FROM [Person].[Person] A
+JOIN [HumanResources].[Employee] B
+ON A.BusinessEntityID = B.BusinessEntityID
+JOIN [HumanResources].[EmployeePayHistory] C
+ON B.BusinessEntityID = C.BusinessEntityID
