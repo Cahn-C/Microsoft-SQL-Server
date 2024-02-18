1a2cfb310e5174be1cb841ad926d179eff3ebb54
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2024-02-17 19:14:45 -0500

    Update AdventureWorks2019Project.sql

--- Projects/AdventureWorks2019Project.sql
+++ Projects/AdventureWorks2019Project.sql
@@ -50,13 +50,13 @@ ON B.BusinessEntityID = C.BusinessEntityID
 
 -- 
 SELECT A.FirstName,
-	   A.LastName,
-	   B.JobTitle,
-	   C.Rate,
-	   AverageRate = AVG(C.Rate) OVER(),
-	   MaximunRate = MAX(C.Rate) OVER(),
-	   DiffFromAvgRate = C.Rate - AVG(C.Rate) OVER(),
-	   PercentageofMaxRate = (C.Rate / MAX(C.Rate) OVER()) * 100
+       A.LastName,
+       B.JobTitle,
+       C.Rate,
+       AverageRate = AVG(C.Rate) OVER(),
+       MaximunRate = MAX(C.Rate) OVER(),
+       DiffFromAvgRate = C.Rate - AVG(C.Rate) OVER(),
+       PercentageofMaxRate = (C.Rate / MAX(C.Rate) OVER()) * 100
 FROM [Person].[Person] A
 JOIN [HumanResources].[Employee] B
 ON A.BusinessEntityID = B.BusinessEntityID
