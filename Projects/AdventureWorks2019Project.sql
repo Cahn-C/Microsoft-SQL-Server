ae7d13cecd115c71210f5d3f49980b3e86d50d81
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2024-02-17 19:14:09 -0500

    Update AdventureWorks2019Project.sql

--- Projects/AdventureWorks2019Project.sql
+++ Projects/AdventureWorks2019Project.sql
@@ -35,12 +35,12 @@ ON B.BusinessEntityID = C.BusinessEntityID
 
 --
 SELECT A.FirstName,
-	   A.LastName,
-	   B.JobTitle,
-	   C.Rate,
-	   AverageRate = AVG(C.Rate) OVER(),
-	   MaximunRate = MAX(C.Rate) OVER(),
-	   DiffFromAvgRate = C.Rate - AVG(C.Rate) OVER()
+       A.LastName,
+       B.JobTitle,
+       C.Rate,
+       AverageRate = AVG(C.Rate) OVER(),
+       MaximunRate = MAX(C.Rate) OVER(),
+       DiffFromAvgRate = C.Rate - AVG(C.Rate) OVER()
 FROM [Person].[Person] A
 JOIN [HumanResources].[Employee] B
 ON A.BusinessEntityID = B.BusinessEntityID
