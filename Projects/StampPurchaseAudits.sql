1985f6e5b037bc88453f51a7a57d5c8783ae6f3a
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2024-02-28 16:42:39 -0500

    Update StampPurchaseAudits.sql

--- Projects/StampPurchaseAudits.sql
+++ Projects/StampPurchaseAudits.sql
@@ -1,3 +1,4 @@
+-- 
 CREATE TABLE [dbo].[tblStampPurchasesAudit] (
 	[StampID] TINYINT NOT NULL,
 	[PurchaseDate] DATE NOT NULL,
@@ -8,6 +9,7 @@ CREATE TABLE [dbo].[tblStampPurchasesAudit] (
 GO
 
 
+-- 
 CREATE TRIGGER TR_tblStampPurchasesAudit_PurchaseAudits ON [dbo].[tblStampPurchases]
 	AFTER INSERT, UPDATE, DELETE
 AS
@@ -19,13 +21,18 @@ BEGIN
 END
 GO
 
+
+-- 
 INSERT INTO [dbo].[tblStampPurchases]([StampID], [PurchaseDate], [PurchasePrice])
 VALUES (7, '2030-01-01', 1234)
 
+-- 
 UPDATE [dbo].[tblStampPurchases] SET [PurchasePrice] = 1235 WHERE StampID = 7
 
+-- 
 DELETE FROM [dbo].[tblStampPurchases] WHERE StampID = 7
 
 
+-- 
 SELECT * FROM [dbo].[tblStampPurchases]
 SELECT * FROM [dbo].[tblStampPurchasesAudit]
